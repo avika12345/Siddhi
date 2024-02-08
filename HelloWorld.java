4eae5bf241941339f98c6919a5f97f43e4545bab
avika12345 <156064919+avika12345@users.noreply.github.com>
2024-02-08 11:15:15 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello WOrld");
+        System.out.println("newwwwwwwww");
+        System.out.println("Changes");
+    }
+}
